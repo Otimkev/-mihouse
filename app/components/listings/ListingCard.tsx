87754920399de72336388e3f77debfcc38256d05
Otimkev <kevinotim4@gmail.com>
2023-05-08 20:31:58 +0300

    Update ListingCard.tsx

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -72,7 +72,8 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
   return (
     <div 
-      onClick={() => router.push(`/listings/${data.id}`)} 
+      onClick={() => console.log("Not implemented!!")} 
+      //onClick={() => router.push(`/listings/${data.id}`)} 
       className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
