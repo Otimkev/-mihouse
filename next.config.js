9492c9f2b35f0056cb159f6a1f6dfedcc6f9dbf6
Otimkev <kevinotim4@gmail.com>
2023-05-14 16:54:24 +0300

    Add vercel configuration file
    Update Vercel CORS policies.

--- next.config.js
+++ next.config.js
@@ -10,14 +10,7 @@ const nextConfig = {
       'lh3.googleusercontent.com',
       'mihousergsouthntest8717.blob.core.windows.net'
     ]
-  },
-  source: "/api/:path*",
-  headers: [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
-    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-  ]
+  }
 }
 
 module.exports = nextConfig
