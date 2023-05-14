75c080955e782a118e328f398c5f299ed035000e
Otimkev <kevinotim4@gmail.com>
2023-05-14 17:10:54 +0300

    Update page.tsx
    Minor clean up

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-  //const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   
 
